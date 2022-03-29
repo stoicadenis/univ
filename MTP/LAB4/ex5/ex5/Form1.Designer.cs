3e64a36a2968c4b9350cb9a4a8f6560cbdc4eff1
Stoica Denis <62135132+stoicadenis@users.noreply.github.com>
2022-03-30 00:17:08 +0300

    Add files via upload

---
+++ MTP/LAB4/ex5/ex5/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ex5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnXML = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnXML
+            // 
+            this.btnXML.Location = new System.Drawing.Point(12, 12);
+            this.btnXML.Name = "btnXML";
+            this.btnXML.Size = new System.Drawing.Size(197, 53);
+            this.btnXML.TabIndex = 0;
+            this.btnXML.Text = "Procesare XML";
+            this.btnXML.UseVisualStyleBackColor = true;
+            this.btnXML.Click += new System.EventHandler(this.btnXML_Click);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 100);
+            this.Controls.Add(this.btnXML);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnXML;
+    }
+}
+
---
+++ MTP/LAB4/ex5/ex5/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ex5
+{ 
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnXML_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            FileStream fs = new FileStream("C:\\Users\\Denis\\Desktop\\MTP\\LAB4\\ex5\\ex5\\tema.xml", FileMode.Open, FileAccess.Read);
+            xmlDoc.Load(fs);
+
+            XmlNode root = xmlDoc.SelectSingleNode("catalog");
+            if(root.HasChildNodes)
+            {
+                for(int i = 0; i < root.ChildNodes.Count; i++)
+                {
+                    double newPrice = double.Parse(root.ChildNodes[i].SelectSingleNode("price").InnerText.ToString(), System.Globalization.CultureInfo.InvariantCulture) + (double.Parse(root.ChildNodes[i].SelectSingleNode("price").InnerText.ToString(), System.Globalization.CultureInfo.InvariantCulture) * 1 / 10);
+                    root.ChildNodes[i].SelectSingleNode("price").InnerText = newPrice.ToString();
+                }
+            }
+            MessageBox.Show("A fost adaugat un adaos de 10% la pret, verificati fisierul XML!");
+            fs.Close();
+
+            fs = new FileStream("C:\\Users\\Denis\\Desktop\\MTP\\LAB4\\ex5\\ex5\\tema.xml", FileMode.Open, FileAccess.Write);
+            xmlDoc.Save(fs);
+            fs.Close();
+            Application.Exit();
+        }
+    }
+}
