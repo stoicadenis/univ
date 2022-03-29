d4e35860af3e4c79a3071fa1f24c59de2a4c5982
Stoica Denis <62135132+stoicadenis@users.noreply.github.com>
2022-03-30 00:16:24 +0300

    Add files via upload

---
+++ MTP/LAB3/ex7/ex7/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace ex7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblDetalii = new System.Windows.Forms.Label();
+            this.lblNume = new System.Windows.Forms.Label();
+            this.lblPost = new System.Windows.Forms.Label();
+            this.lblCNP = new System.Windows.Forms.Label();
+            this.lblDataNasterii = new System.Windows.Forms.Label();
+            this.lblVarsta = new System.Windows.Forms.Label();
+            this.txtNume = new System.Windows.Forms.TextBox();
+            this.txtPost = new System.Windows.Forms.TextBox();
+            this.txtCNP = new System.Windows.Forms.TextBox();
+            this.txtDataNasterii = new System.Windows.Forms.TextBox();
+            this.txtVarsta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Echipa";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(59, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Echipa noua";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(59, 42);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(333, 354);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(317, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Jucator nou";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblDetalii
+            // 
+            this.lblDetalii.AutoSize = true;
+            this.lblDetalii.Location = new System.Drawing.Point(455, 81);
+            this.lblDetalii.Name = "lblDetalii";
+            this.lblDetalii.Size = new System.Drawing.Size(74, 13);
+            this.lblDetalii.TabIndex = 5;
+            this.lblDetalii.Text = "Detalii Jucator";
+            this.lblDetalii.Visible = false;
+            // 
+            // lblNume
+            // 
+            this.lblNume.AutoSize = true;
+            this.lblNume.Location = new System.Drawing.Point(486, 111);
+            this.lblNume.Name = "lblNume";
+            this.lblNume.Size = new System.Drawing.Size(35, 13);
+            this.lblNume.TabIndex = 6;
+            this.lblNume.Text = "Nume";
+            this.lblNume.Visible = false;
+            // 
+            // lblPost
+            // 
+            this.lblPost.AutoSize = true;
+            this.lblPost.Location = new System.Drawing.Point(493, 138);
+            this.lblPost.Name = "lblPost";
+            this.lblPost.Size = new System.Drawing.Size(28, 13);
+            this.lblPost.TabIndex = 7;
+            this.lblPost.Text = "Post";
+            this.lblPost.Visible = false;
+            // 
+            // lblCNP
+            // 
+            this.lblCNP.AutoSize = true;
+            this.lblCNP.Location = new System.Drawing.Point(492, 166);
+            this.lblCNP.Name = "lblCNP";
+            this.lblCNP.Size = new System.Drawing.Size(29, 13);
+            this.lblCNP.TabIndex = 8;
+            this.lblCNP.Text = "CNP";
+            this.lblCNP.Visible = false;
+            // 
+            // lblDataNasterii
+            // 
+            this.lblDataNasterii.AutoSize = true;
+            this.lblDataNasterii.Location = new System.Drawing.Point(455, 195);
+            this.lblDataNasterii.Name = "lblDataNasterii";
+            this.lblDataNasterii.Size = new System.Drawing.Size(66, 13);
+            this.lblDataNasterii.TabIndex = 9;
+            this.lblDataNasterii.Text = "Data nasterii";
+            this.lblDataNasterii.Visible = false;
+            // 
+            // lblVarsta
+            // 
+            this.lblVarsta.AutoSize = true;
+            this.lblVarsta.Location = new System.Drawing.Point(484, 224);
+            this.lblVarsta.Name = "lblVarsta";
+            this.lblVarsta.Size = new System.Drawing.Size(37, 13);
+            this.lblVarsta.TabIndex = 10;
+            this.lblVarsta.Text = "Varsta";
+            this.lblVarsta.Visible = false;
+            // 
+            // txtNume
+            // 
+            this.txtNume.Location = new System.Drawing.Point(537, 108);
+            this.txtNume.Name = "txtNume";
+            this.txtNume.ReadOnly = true;
+            this.txtNume.Size = new System.Drawing.Size(157, 20);
+            this.txtNume.TabIndex = 11;
+            this.txtNume.Visible = false;
+            // 
+            // txtPost
+            // 
+            this.txtPost.Location = new System.Drawing.Point(537, 135);
+            this.txtPost.Name = "txtPost";
+            this.txtPost.ReadOnly = true;
+            this.txtPost.Size = new System.Drawing.Size(157, 20);
+            this.txtPost.TabIndex = 12;
+            this.txtPost.Visible = false;
+            // 
+            // txtCNP
+            // 
+            this.txtCNP.Location = new System.Drawing.Point(537, 163);
+            this.txtCNP.Name = "txtCNP";
+            this.txtCNP.ReadOnly = true;
+            this.txtCNP.Size = new System.Drawing.Size(157, 20);
+            this.txtCNP.TabIndex = 13;
+            this.txtCNP.Visible = false;
+            // 
+            // txtDataNasterii
+            // 
+            this.txtDataNasterii.Location = new System.Drawing.Point(537, 192);
+            this.txtDataNasterii.Name = "txtDataNasterii";
+            this.txtDataNasterii.ReadOnly = true;
+            this.txtDataNasterii.Size = new System.Drawing.Size(157, 20);
+            this.txtDataNasterii.TabIndex = 14;
+            this.txtDataNasterii.Visible = false;
+            // 
+            // txtVarsta
+            // 
+            this.txtVarsta.Location = new System.Drawing.Point(537, 221);
+            this.txtVarsta.Name = "txtVarsta";
+            this.txtVarsta.ReadOnly = true;
+            this.txtVarsta.Size = new System.Drawing.Size(157, 20);
+            this.txtVarsta.TabIndex = 15;
+            this.txtVarsta.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtVarsta);
+            this.Controls.Add(this.txtDataNasterii);
+            this.Controls.Add(this.txtCNP);
+            this.Controls.Add(this.txtPost);
+            this.Controls.Add(this.txtNume);
+            this.Controls.Add(this.lblVarsta);
+            this.Controls.Add(this.lblDataNasterii);
+            this.Controls.Add(this.lblCNP);
+            this.Controls.Add(this.lblPost);
+            this.Controls.Add(this.lblNume);
+            this.Controls.Add(this.lblDetalii);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "S";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblDetalii;
+        private System.Windows.Forms.Label lblNume;
+        private System.Windows.Forms.Label lblPost;
+        private System.Windows.Forms.Label lblCNP;
+        private System.Windows.Forms.Label lblDataNasterii;
+        private System.Windows.Forms.Label lblVarsta;
+        private System.Windows.Forms.TextBox txtNume;
+        private System.Windows.Forms.TextBox txtPost;
+        private System.Windows.Forms.TextBox txtCNP;
+        private System.Windows.Forms.TextBox txtDataNasterii;
+        private System.Windows.Forms.TextBox txtVarsta;
+    }
+}
+
---
+++ MTP/LAB3/ex7/ex7/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ex7
+{
+    public partial class Form1 : Form
+    {
+        private string[] subDirs = Directory.GetDirectories("C:\\Users\\Denis\\Desktop\\MTP\\LAB3\\ex7\\ex7\\Echipa");
+        private string[] fisiere;
+
+        private TextBox txtEchipaNoua;
+        private TextBox[] txtJucator = new TextBox[3];
+        private int x = 0, y = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (string currSubDir in subDirs)
+                comboBox1.Items.Add(currSubDir.Split('\\').Last());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblCNP.Visible = false;
+            lblDataNasterii.Visible = false;
+            lblDetalii.Visible = false;
+            lblNume.Visible = false;
+            lblPost.Visible = false;
+            lblVarsta.Visible = false;
+            txtCNP.Visible = false;
+            txtDataNasterii.Visible = false;
+            txtNume.Visible = false;
+            txtPost.Visible = false;
+            txtVarsta.Visible = false;
+            flowLayoutPanel1.Controls.Clear();
+            fisiere = Directory.GetFiles("C:\\Users\\Denis\\Desktop\\MTP\\LAB3\\ex7\\ex7\\Echipa\\" + comboBox1.Text);
+            foreach (string fis in fisiere)
+            {
+                string[] nume = File.ReadAllLines(fis);
+                Button btn = new Button();
+                btn.Text = nume[0];
+                btn.SetBounds(x, y, 200, 40);
+                btn.Click += new EventHandler(btnClick_Jucator);
+                flowLayoutPanel1.Controls.Add(btn);
+                y += 42;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblCNP.Visible = false;
+            lblDataNasterii.Visible = false;
+            lblDetalii.Visible = false;
+            lblNume.Visible = false;
+            lblPost.Visible = false;
+            lblVarsta.Visible = false;
+            txtCNP.Visible = false;
+            txtDataNasterii.Visible = false;
+            txtNume.Visible = false;
+            txtPost.Visible = false;
+            txtVarsta.Visible = false;
+            flowLayoutPanel1.Controls.Clear();
+
+            txtEchipaNoua = new TextBox();
+            txtEchipaNoua.SetBounds(0, 0, 200, 35);
+
+            Button btnEchipaNoua = new Button();
+            btnEchipaNoua.Text = "Adauga Echipa";
+            btnEchipaNoua.SetBounds(0, 40, 200, 35);
+            btnEchipaNoua.Click += new EventHandler(btnClick_EchipaNoua);
+
+            flowLayoutPanel1.Controls.Add(txtEchipaNoua);
+            flowLayoutPanel1.Controls.Add(btnEchipaNoua);
+        }
+
+        private void btnClick_EchipaNoua(object sender, EventArgs e)
+        {
+            DirectoryInfo di = Directory.CreateDirectory("C:\\Users\\Denis\\Desktop\\MTP\\LAB3\\ex7\\ex7\\Echipa\\" + txtEchipaNoua.Text);
+            comboBox1.Items.Add(txtEchipaNoua.Text);
+            comboBox1.ResetText();
+            flowLayoutPanel1.Controls.Clear();
+            MessageBox.Show("Echipa adaugata cu succes");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.Text.Equals(""))
+            {
+                MessageBox.Show("Eroare la comboBox, nu ati selectat echipa!");
+            }
+            else
+            {
+                lblCNP.Visible = false;
+                lblDataNasterii.Visible = false;
+                lblDetalii.Visible = false;
+                lblNume.Visible = false;
+                lblPost.Visible = false;
+                lblVarsta.Visible = false;
+                txtCNP.Visible = false;
+                txtDataNasterii.Visible = false;
+                txtNume.Visible = false;
+                txtPost.Visible = false;
+                txtVarsta.Visible = false;
+                flowLayoutPanel1.Controls.Clear();
+
+                Label[] lblJucator = new Label[3];
+                lblJucator[0] = new Label();
+                lblJucator[1] = new Label();
+                lblJucator[2] = new Label();
+                lblJucator[0].Text = "Nume";
+                lblJucator[1].Text = "Post";
+                lblJucator[2].Text = "CNP";
+                lblJucator[0].SetBounds(0, 0, 35, 20);
+                lblJucator[1].SetBounds(0, 22, 30, 20);
+                lblJucator[2].SetBounds(0, 44, 30, 20);
+
+                txtJucator[0] = new TextBox();
+                txtJucator[1] = new TextBox();
+                txtJucator[2] = new TextBox();
+                txtJucator[0].SetBounds(32, 0, 150, 30);
+                txtJucator[1].SetBounds(32, 22, 150, 30);
+                txtJucator[2].SetBounds(32, 44, 150, 30);
+
+                Button btnAddJucator = new Button();
+                btnAddJucator.Text = "Adauga Jucator";
+                //btnAddJucator.Click += (se, ee) => { btnClick_JucatorNou(sender, e, txtJucator[2].Text); };
+                btnAddJucator.Click += new EventHandler(btnClick_JucatorNou);
+                btnAddJucator.SetBounds(0, 0, 150, 40);
+                flowLayoutPanel1.Controls.Add(lblJucator[0]);
+                flowLayoutPanel1.Controls.Add(txtJucator[0]);
+                flowLayoutPanel1.Controls.Add(lblJucator[1]);
+                flowLayoutPanel1.Controls.Add(txtJucator[1]);
+                flowLayoutPanel1.Controls.Add(lblJucator[2]);
+                flowLayoutPanel1.Controls.Add(txtJucator[2]);
+                flowLayoutPanel1.Controls.Add(btnAddJucator);
+            }
+        }
+
+        private void btnClick_JucatorNou(object sender, EventArgs e)
+        {
+            string docPath = @"C:\Users\Denis\Desktop\MTP\LAB3\ex7\ex7\Echipa\" + comboBox1.Text + @"\" + txtJucator[2].Text + ".txt";
+            using (StreamWriter writer = new StreamWriter(docPath))
+            {
+                writer.WriteLine(txtJucator[0].Text);
+                writer.WriteLine(txtJucator[1].Text);
+            }
+           
+            MessageBox.Show("Jucatorul s-a adaugat cu succes!");
+            comboBox1.ResetText();
+            flowLayoutPanel1.Controls.Clear();
+        }
+
+        private void btnClick_Jucator(object sender, EventArgs e)
+        {
+            lblCNP.Visible = true;
+            lblDataNasterii.Visible = true;
+            lblDetalii.Visible = true;
+            lblNume.Visible = true;
+            lblPost.Visible = true;
+            lblVarsta.Visible = true;
+            txtCNP.Visible = true;
+            txtDataNasterii.Visible = true;
+            txtNume.Visible = true;
+            txtPost.Visible = true;
+            txtVarsta.Visible = true;
+
+            Button btnClick = (Button)sender;
+            string[] jucator = new string[2];
+            foreach (string fis in fisiere)
+            {
+                jucator = File.ReadAllLines(fis);
+                if (jucator[0].Equals(btnClick.Text))
+                {
+                    txtNume.Text = jucator[0];
+                    txtPost.Text = jucator[1];
+                    txtCNP.Text = fis.Split('\\').Last().Split('.').First();
+                    break;
+                }        
+            }
+            string[] buffer = Regex.Split(txtCNP.Text, string.Empty);
+            
+            txtDataNasterii.Text = buffer[6] + buffer[7] + "." + buffer[4] + buffer[5] + "." + (buffer[1] == "1" ? "19" : "20") + buffer[2] + buffer[3];
+
+            int buff = 2021 - int.Parse((buffer[1] == "1" ? "19" : "20") + buffer[2] + buffer[3]);
+            txtVarsta.Text = buff.ToString();
+        }
+    }
+}
