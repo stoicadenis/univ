eb442a97fa8eaaea1ec6c0202f858d37da350c0d
Stoica Denis <62135132+stoicadenis@users.noreply.github.com>
2022-03-30 00:14:55 +0300

    Add files via upload

---
+++ MTP/LAB3/ex6/ex6/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ex6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // txtM
+            // 
+            this.txtM.Location = new System.Drawing.Point(34, 12);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(71, 20);
+            this.txtM.TabIndex = 0;
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(34, 48);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(71, 20);
+            this.txtN.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "M";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "N";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(138, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(585, 426);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.txtM);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ MTP/LAB3/ex6/ex6/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex6
+{
+    public partial class Form1 : Form
+    {
+        private int x, y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtM.Text.Equals("") || txtN.Text.Equals(""))
+            {
+                MessageBox.Show("Nu ati introdus nr de linii/ coloane!");
+            }
+            else
+            {
+                x = y = 0;
+                for(int i = 0; i < int.Parse(txtM.Text); i++)
+                {
+                    for (int j = 0; j < int.Parse(txtN.Text); j++)
+                    {
+                        Button btn = new Button();
+                        btn.Text = i + " " + j;
+                        btn.SetBounds(x, y, 500/int.Parse(txtM.Text), 400/int.Parse(txtN.Text));
+                        flowLayoutPanel1.Controls.Add(btn);
+                        y += 500 / int.Parse(txtM.Text) + 10;
+                    }
+                    x += 400 / int.Parse(txtN.Text) + 10;
+                }
+            }
+        }
+
+    }
+}
