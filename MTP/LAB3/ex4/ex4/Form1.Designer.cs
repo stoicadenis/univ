ef6425f8bfbf852b008718f0a69de0b3a72c7b53
Stoica Denis <62135132+stoicadenis@users.noreply.github.com>
2022-03-30 00:11:04 +0300

    Add files via upload

---
+++ MTP/LAB3/ex4/ex4/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ex4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboControale = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboControale
+            // 
+            this.comboControale.FormattingEnabled = true;
+            this.comboControale.Items.AddRange(new object[] {
+            "Button",
+            "TextBox",
+            "ComboBox"});
+            this.comboControale.Location = new System.Drawing.Point(13, 13);
+            this.comboControale.Name = "comboControale";
+            this.comboControale.Size = new System.Drawing.Size(177, 21);
+            this.comboControale.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(229, 13);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(559, 425);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(13, 41);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(177, 39);
+            this.buttonSubmit.TabIndex = 2;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.comboControale);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboControale;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonSubmit;
+    }
+}
+
---
+++ MTP/LAB3/ex4/ex4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex4
+{
+    public partial class Form1 : Form
+    {
+        private int position = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            if (comboControale.Text.Equals("Button"))
+            {
+                Button button = new Button();
+                button.Text = "Button";
+                button.SetBounds(0, position, 200, 30);
+                position += 32;
+                flowLayoutPanel1.Controls.Add(button);
+            }
+            else if (comboControale.Text.Equals("ComboBox"))
+            {
+                ComboBox combo = new ComboBox();
+                combo.SetBounds(0, position, 200, 30);
+                position += 32;
+                flowLayoutPanel1.Controls.Add(combo);
+            }
+            else if (comboControale.Text.Equals("TextBox"))
+            {
+                TextBox textbox = new TextBox();
+                textbox.SetBounds(0, position, 200, 30);
+                position += 32;
+                flowLayoutPanel1.Controls.Add(textbox);
+            }
+            else
+                MessageBox.Show("Nu ati selectat nici un element!");
+            
+        }
+    }
+}
